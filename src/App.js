81b5d5272cc8655c380c78e2c5b55de494a5438c
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-08 11:31:28 -0800

    Added a Home and an About path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
@@ -12,7 +13,7 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={About} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/search" component={Search} />
         </Wrapper>
