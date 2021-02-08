11f0d8b409dd212f26de02493fc39bad5877a3bc
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-08 11:55:18 -0800

    Added the components folder from the pupster app

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+const URL = "https://randomuser.me/api/";
+// const APIKEY = "";
+
+//exporting default ajax call as axios
+export default {
+  search: function (query) {
+    return axios.get(URL + query);
+  },
+};
