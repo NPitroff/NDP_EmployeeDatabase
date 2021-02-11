c5e5a200e3bec31f87bc3ba8a37b749c84abdd72
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-10 18:30:10 -0800

    Changed the name on the search page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Pupster
+        NDPendentCoder Employee Database
       </Link>
       <div>
         <ul className="navbar-nav">
